e606a611395ea5335efdb3c3bcf891dbbaa802fc
cigiol <cigidemenolur@hotmail.com>
2024-10-19 22:33:17 +0300

    Card Heights fixed

--- src/pages/Modules/CardList/Card.jsx
+++ src/pages/Modules/CardList/Card.jsx
@@ -2,12 +2,10 @@ import Button from "@components/Button";
 
 const Card = ({ id = "", image = "", name = "", price = "" }) => {
   return (
-    <div className="flex flex-col bg-white p-3 gap-3">
+    <div className="flex flex-col justify-between bg-white p-3 gap-3">
       <img src={image} />
-      <div className="flex flex-col">
-        <span className="text-primary">{price} ₺</span>
-        <span className="text-black">{name}</span>
-      </div>
+      <span className="text-primary">{price} ₺</span>
+      <span className="text-black">{name}</span>
       <Button buttonText="Add To Cart" buttonClass="rounded-md p-2" />
     </div>
   );
