55db633d76d2098ecc4d86483628eefb64fdbbae
cigiol <cigidemenolur@hotmail.com>
2024-10-19 14:26:10 +0300

    structure created , tailwind , rtk , axios , router added

---
+++ src/routing/Router.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Views/Home.jsx";
+import Product from "../pages/Views/Product.jsx";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
