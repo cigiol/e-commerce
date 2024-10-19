78e4050242b5ab430cf7d7a9767c5a2f280a08ed
cigiol <cigidemenolur@hotmail.com>
2024-10-19 14:57:51 +0300

    utils added

---
+++ src/utils/axiosBaseQuery.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+const API_BASE_URL = process.env.API_BASE_URL;
+
+const axiosParameterBuilder = async (config) => {
+  const { method, headers = {}, data = {} } = config;
+  // #region HEADERS
+  const headerParameters = {
+    Accept: "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+    ...headers,
+  };
+  // #endregion HEADERS
+
+  // #region DATA
+  let requestData = data;
+
+  if (["POST", "PATCH", "PUT", "DELETE"].includes(method)) {
+    requestData = JSON.stringify(data);
+  }
+  // #endregion DATA
+
+  return {
+    data: requestData,
+    headers: headerParameters,
+    method,
+  };
+};
+
+const apiURLBuilder = (params) => {
+  const { baseUrl = API_BASE_URL, path = "" } = params;
+  return `${baseUrl}/${path}`;
+};
+
+export const axiosBaseQuery =
+  ({ baseUrl } = { baseUrl: API_BASE_URL }) =>
+  async (queryParams) => {
+    try {
+      const { path, ...rest } = queryParams;
+      const params = await axiosParameterBuilder(rest);
+      const requestUrl = apiURLBuilder({ baseUrl, path });
+      const { data } = await axios(requestUrl, params);
+      return { data };
+    } catch (axiosError) {
+      const err = axiosError;
+      return {
+        error: { status: err.response?.status, data: err.response?.data },
+      };
+    }
+  };
+
+export const apiResHandler = (
+  promise,
+  callback = () => {},
+  failCallback = () => {}
+) => {
+  promise
+    .then((res) => {
+      const { data, error } = res;
+      if (!data && error)
+        throw new Error(error?.data?.Message || "Something went wrong!");
+      const { data: resData, message, status = true } = data;
+      if (!status) {
+        failCallback?.({ message });
+        return;
+      }
+      callback?.(resData);
+    })
+    .catch((err) => {
+      const { message } = err;
+      failCallback?.({ errMsg: message });
+    });
+};
